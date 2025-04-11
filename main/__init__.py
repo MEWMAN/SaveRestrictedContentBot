46a48cd901c1d3ed13da61800bbdd4f7789ee22c
MEWMAN <asham.www.com@gmail.com>
2025-04-11 14:25:10 +0300

    __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -15,9 +15,9 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 API_ID = config("21248884", default=None, cast=int)
 API_HASH = config("25c51c205e114ff4574b095cdb924752", default=None)
 BOT_TOKEN = config("7952935242:AAF-0cqZbfGrFkHh_3Z_BjmkUTL3yl39WYU", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None, cast=int)
+SESSION = config("1BJWap1sBu6a-pbD0Kr-49KoVZAV-04CVgTCv3YNwfhxqHHBVp1dP5ENuJmHcnnNXc0wHitgU4JcSmCasCUFpt32HOzwpfdB9D7SfYFaTbgjcszWoIYiGt0eAypTC7oJqDzxiV--b9vdkawzdQBC7kyXw89VMfICi4h5Uh5kSopkH55kNlyU_II_VDlwisQYYcHyguvXluiq1E5yNsuHUY3ilKq1i0TAAWP6RHkHdckg0Q5gr-o_Y2e6qvPVobcbO4iqIG3VOnrSJPJQS8xz8bGXjXz51LYJFExzxoqvl84ARewuhgKAZBBdY5aOhL5sLbX4JciDyvASnIxzZBTwqApaRNtAXX98=", default=None)
+FORCESUB = config("d4q44", default=None)
+AUTH = config("8185475054", default=None, cast=int)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
